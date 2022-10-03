0c8d095d0ecbac06b0006f07c5c2f33bc0722150
abhinavvermacbse <abhinavvermacbse@gmail.com>
2022-10-03 22:25:59 +0530

    Task 9 done by Abhinav Verma

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+  
+// fs.mkdirSync("thapa");
+
+// fs.writeFileSync("bio.txt","My name is Abhinav Verma");
+
+// fs.appendFileSync('bio.txt','and i am the best');
+
+// const data= fs.readFileSync('bio.txt');
+// console.log(data);
+
+// fs.renameSync("bio.txt","myBio.txt");
+
+// fs.unlinkSync("myBio.txt");
+
+
+
